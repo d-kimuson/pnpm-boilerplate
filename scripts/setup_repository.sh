395fa10493f89dab0579c1c575e3c46d934915c7
d-kimsuon <dev.kimuson@gmail.com>
2023-06-13 13:00:57 +0900

    feat(*): setup スクリプト修正

--- scripts/setup_repository.sh
+++ scripts/setup_repository.sh
@@ -2,10 +2,17 @@
 
 set -eux
 
-direnv allow
+PROJECT_DIR_NAME=$(pwd | rev | cut -d '/' -f 1 | rev)
+
+sed -i -e "s/pnpm-boilerplate/$PROJECT_DIR_NAME/g" package.json
+
 nodenv install -s
 corepack enable pnpm
 ./scripts/update_pnpm.sh
 ./scripts/setup.sh
 
 rm ./scripts/setup_repository.sh
+rm -rf .git
+git init
+git add .
+git commit -m"auto(*): Initialize Repository from boilerplate"
