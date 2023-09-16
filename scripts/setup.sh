4bc30ff03676612207e1155b1acd05556440dfab
d-kimsuon <dev.kimuson@gmail.com>
2023-09-16 23:51:00 +0900

    ci(ci): これならいけるか

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,25 +2,17 @@
 
 set -eux
 
-packages="eslint-config-custom tsconfig"
+# example: "backend frontend"
 apps=""
 
 nodenv install -s
 corepack enable
 corepack prepare pnpm@8.7.5 --activate
 
-pnpm i
-
-for package in $packages; do
-  cd packages/$package
-  nodenv install -s
-  pnpm i
-  cd -
-done
-
 for app in $apps; do
   cd apps/$app
   nodenv install -s
-  pnpm i
   cd -
 done
+
+pnpm --filter "*" i
